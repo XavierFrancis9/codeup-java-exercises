20f7b98bca10821637f412cf5cc7714efb9ca7f6
Xavier Francis <james.x.francis9@gmail.com>
2023-04-14 14:38:24 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+//shortcut psvm || main + tab
+public class HelloWorld {
+    public static void main(String[] args){
+        // this logs things into the command line
+        // strings in java must be in double quotes! (single quotes are reserved for single characters)
+        // MUST use semicolons are end of statements IOT compile
+        // short cut is sout + tab
+        System.out.println("Hello, World!");
+    }
+}
